61c85387791edaab43f4b9e6874e3c672cd0163b
nelu <wf.nelu@yandex.ru>
2020-03-03 12:28:05 +0100

    tests

---
+++ exo4.py
@@ -0,0 +1,6 @@
+chaine = input("Saisissez une chaîne de caractères : ")
+
+if 'e' in chaine:
+    print("La chaîne de texte contient la lettre 'e'.")
+else:
+    print("La chaîne de texte ne contient pas la lettre 'e'.")
