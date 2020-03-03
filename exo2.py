9250ac6cfbdc936ce84fa3cc0fcc864f3a87424c
nelu <wf.nelu@yandex.ru>
2020-03-03 11:38:02 +0100

    list

---
+++ exo2.py
@@ -0,0 +1,14 @@
+a = []
+
+depart = int( input( "Saisissez un nombre de départ : " ) )
+arrive = int( input( "Saisissez un nombre d'arrivé : " ) )
+pas = int( input( "Saissisez un pas : " ) )
+
+a.append( depart )
+a.append( arrive )
+a.append( pas )
+
+a.extend( list( range( a[0], a[1], a[2] ) ) )
+print( "Liste: ", a )
+
+print( "Pas des derniers éléments de la liste : ", a[-1 * pas:] )
