1a36bace6d7dc14421ada6a8b2d68694017f2bba
nelu <wf.nelu@yandex.ru>
2020-03-03 10:22:00 +0100

    start python

---
+++ exo1.py
@@ -0,0 +1,6 @@
+nb1 = input( "Saisissez un premier nombre : " )
+nb2 = input( "Saisissez un second nombre : " )
+
+print( "Voici les deux nobmres entrés", nb1, nb2 )
+
+print( "Somme : ", int(nb1) + int(nb2) )
